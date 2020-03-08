3674449af25342281ee38e10c6cf473bf8ec152e
fsfe-server <fsfe@server.com>
2020-03-08 01:59:41 +0000

    Added GET /demo

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => {
   res.send('Nice work, Jonathan!');
 });
 
+app.get('/demo', (req, res) => {
+  res.set('X-Full-Stack', '4-life');
+  res.status(418);
+  res.send('I prefer coffee');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
